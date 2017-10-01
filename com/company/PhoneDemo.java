9db0da7032629cce0e50cb68a7e42562ec3fd075
Victor Kucherenko <Ku4erenko.V.V@yandex.ru>
2017-10-01 11:36:25 +0300

    comments

--- com/company/PhoneDemo.java
+++ com/company/PhoneDemo.java
@@ -5,13 +5,13 @@ package com.company;
  */
 public class PhoneDemo {
     public static void main(String args[]) {
-        Phone phonchik1 = new Phone();
-        Phone phonchik2 = new Phone();
+        Phone phonchik1 = new Phone(380503997423L,"Samsung",10.2);
+        Phone phonchik2 = new Phone(8098632523L,"");
         Phone phonchik3 = new Phone();
 
-        phonchik1.number = 380503997423L;
-        phonchik1.model = "Samsung";
-        phonchik1.weight = 10.2;
+       // phonchik1.number = 380503997423L;
+       // phonchik1.model = "Samsung";
+      //  phonchik1.weight = 10.2;
         System.out.println("Телефон №1 имеет следующие параметры: " + phonchik1.number + " " + phonchik1.model + " " + phonchik1.weight);
         phonchik1.receiveCall("Кто-то");
         System.out.println(phonchik1.getPhoneNumber());
