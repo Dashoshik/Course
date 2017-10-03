eaa67142f924c63bd50c6a4ae71340d38caab2df
Dashoshik <daryastarkova01@gmailcom>
2017-10-03 21:28:53 +0300

    Update

---
+++ src/com/company/Person.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Person {
+    String fullName;
+    int age;
+    move();
+    talk();
+
+    Person(String n,int a){
+        fullName = n;
+        age = a;
+    }
+
+    Person ();
+
+        Person boy = new Person();
+        Person girl = new Person (Kate, 30);
+}
