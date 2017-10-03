62b0695b8c294d0d87dbc269bea1fb2bcce4eb9d
Dashoshik <daryastarkova01@gmailcom>
2017-10-03 21:29:36 +0300

    Update

--- src/com/company/Person.java
+++ src/com/company/Person.java
@@ -3,16 +3,18 @@ package com.company;
 public class Person {
     String fullName;
     int age;
+
     move();
+
     talk();
 
-    Person(String n,int a){
+    Person(String n, int a) {
         fullName = n;
         age = a;
     }
 
-    Person ();
+    Person();
 
-        Person boy = new Person();
-        Person girl = new Person (Kate, 30);
+    Person boy = new Person();
+    Person girl = new Person(Kate, 30);
 }
