5f1bf2e0102ee1389ae52842ed0669dd0b004aec
andreitk2017 <ivanes8400@mail.ru>
2017-10-04 19:30:07 +0300

    updates of Person class

--- src/com/company/Person.java
+++ src/com/company/Person.java
@@ -4,17 +4,28 @@ public class Person {
     String fullName;
     int age;
 
-    move();
+    void move() {
+        System.out.println(fullName + " is moving");
+    }
 
-    talk();
+    void talk() {
+        System.out.println("it's age of person " + age);
+    }
 
     Person(String n, int a) {
         fullName = n;
         age = a;
     }
 
-    Person();
+    Person() {
+    }
 
-    Person boy = new Person();
-    Person girl = new Person(Kate, 30);
+
+    public static void main(String[] args) {
+        Person boy = new Person();
+        Person girl = new Person("Kate", 30);
+        boy.talk();
+        girl.move();
+    }
 }
+
