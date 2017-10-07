419ba2ff0d81a28e0789a9f7b3e0cccdf0467687
Dashoshik <daryastarkova01@gmailcom>
2017-10-07 22:12:38 +0300

    Update

---
+++ src/com/company/Circle.java
@@ -0,0 +1,27 @@
+package com.company;
+
+public class Circle {
+    double radius;
+    double pi;
+
+    void square() {
+        System.out.println("Площадь равна " + (pi * radius * radius));
+    }
+
+    void length() {
+        System.out.println("Длина окружности равна " + (2 * pi * radius));
+    }
+
+
+    Circle(double r, double pi) {
+        radius = r;
+        pi = pi;
+    }
+
+    public static void main(String[] args) {
+        Circle krug1 = new Circle(10.2, 3.14);
+        Circle krug2 = new Circle(4.96, 3.14);
+        krug1.square();
+        krug2.length();
+    }
+}
