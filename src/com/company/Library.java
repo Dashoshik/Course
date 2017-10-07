314695017a558dbd472b8e372858a7d8f959cb1a
Dashoshik <daryastarkova01@gmailcom>
2017-10-07 21:24:17 +0300

    Update

---
+++ src/com/company/Library.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Library {
+    String name;
+    int numberTicket;
+    String faculty;
+    String dateOfBirth;
+    long phoneNumber;
+
+
+    public static void main(String args[]) {
+        Library reader1 = new Library ();
+        Library reader2 = new Library ();
+
+    }
+
+    )
