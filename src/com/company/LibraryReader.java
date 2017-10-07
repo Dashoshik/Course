c7123cf2e13d55215636b37431016bbd2ad9c896
Dashoshik <daryastarkova01@gmailcom>
2017-10-07 21:50:47 +0300

    Update

--- src/com/company/LibraryReader.java
+++ src/com/company/LibraryReader.java
@@ -7,6 +7,10 @@ public class LibraryReader {
     String dateOfBirth;
     long phoneNumber;
 
+    LibraryReader() {
+
+    }
+
 
     public static void main(String args[]) {
         LibraryReader reader1 = new LibraryReader();
@@ -14,7 +18,7 @@ public class LibraryReader {
         String infoReader[][] = {
                 {"Kate", "RT-2506", "economic", "10.05.1999", "096-74-65-222"},
                 {"John", "OP - 3296", "economic", "01.08.1998", "050-98-42-379"},
-        System.out.println(infoReader);
+                System.out.println(infoReader);
         };
     }
 
