179cf60210b5ddaf432a0218987b0d735a39e152
Dashoshik <daryastarkova01@gmailcom>
2017-10-09 21:13:11 +0300

    добавлен hasNextDouble

--- src/lesson3/Ternary.java
+++ src/lesson3/Ternary.java
@@ -7,18 +7,20 @@ public class Ternary {
         Scanner scan = new Scanner(System.in);
         System.out.println("Введите три числа");
         double a = 0.0, b = 0.0, c = 0.0;
-        a = scan.nextDouble();
-        b = scan.nextDouble();
-        c = scan.nextDouble();
-        double modulA = a < 0 ? -a : a;
-        double modulB = b < 0 ? -b : b;
-        double modulC = c < 0 ? -c : c;
-        if (modulA < modulB && modulA < modulC) {
-            System.out.println(modulA);
-        } else if (modulB < modulA && modulB < modulC) {
-            System.out.println(modulB);
-        } else {
-            System.out.println(modulC);
+        if (scan.hasNextDouble()) {
+            a = scan.nextDouble();
+            b = scan.nextDouble();
+            c = scan.nextDouble();
+            double modulA = a < 0 ? -a : a;
+            double modulB = b < 0 ? -b : b;
+            double modulC = c < 0 ? -c : c;
+            if (modulA < modulB && modulA < modulC) {
+                System.out.println(modulA);
+            } else if (modulB < modulA && modulB < modulC) {
+                System.out.println(modulB);
+            } else {
+                System.out.println(modulC);
+            }
         }
     }
 }
