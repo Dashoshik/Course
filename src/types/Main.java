76cf481d4ec62e87f931598e72b3c7c8d7fa01e7
Dashoshik <daryastarkova01@gmailcom>
2017-10-09 21:00:55 +0300

    преобразования

---
+++ src/types/Main.java
@@ -0,0 +1,37 @@
+package types;
+
+public class Main {
+
+    public static void main(String[] args) {
+        byte i = 2,  z =3;
+        int b = i + 2;
+        z *= 2;
+        System.out.println(b + z);
+        byte k = 4;
+        short a = (short) (k * 2);
+        System.out.println(a);
+        byte o = 3;
+        byte pp = (byte) o;
+        System.out.println(pp);
+        byte kk = 3;
+        long rr = 3*2;
+        System.out.println(rr);
+        byte u=14;
+        float uu = u;
+        System.out.println(uu);
+        byte c =5;
+        double c2 = c*0.33;
+        System.out.println(c2);
+        byte g = -31;
+        char gg = (char)g;
+        System.out.println(gg);
+    }
+    public static void second(String[] args) {
+        long ar = 987;
+        byte ar2 = (byte)(ar-87);
+        System.out.println(ar2);
+        long art = 78;
+        art *=2;
+        System.out.println(art);
+    }
+}
