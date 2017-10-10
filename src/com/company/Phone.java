ffd4bc4548d4b73457511021267c3076efd665e7
Dashoshik <daryastarkova01@gmailcom>
2017-10-10 19:53:15 +0300

    отформатирован Phone

--- src/com/company/Phone.java
+++ src/com/company/Phone.java
@@ -4,7 +4,7 @@ package com.company;
  * Created by student on 01-Oct-17.
  */
 public class Phone {
- private long number;
+    private long number;
     private String model;
     private double weight;
 
@@ -14,12 +14,30 @@ public class Phone {
         model = m;
         weight = w;
     }
-    public long getNumber(){return number;}
-    public void setNumber (long number){this.number = number;}
-    public String getModel(){return model;}
-    public void setModel (String model){this.model = model;}
-    public double getWeight(){return weight;}
-    public void setWeight (double weight){this.weight = weight;}
+
+    public long getNumber() {
+        return number;
+    }
+
+    public void setNumber(long number) {
+        this.number = number;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
 
     Phone() {
         System.out.println("Описание телефона: ");
@@ -28,16 +46,18 @@ public class Phone {
     void receiveCall(String name) {
         System.out.println("Звонит " + name);
     }
-    void receiveCall (String f, long b){
-        System.out.println("Звонит " + f + ",номер: "+b);
+
+    void receiveCall(String f, long b) {
+        System.out.println("Звонит " + f + ",номер: " + b);
     }
 
     long getPhoneNumber() {
         return number;
     }
 
-    void sendMessage (long... array) {
-        for (long num:array){
-        System.out.println("Message from " + num);}
+    void sendMessage(long... array) {
+        for (long num : array) {
+            System.out.println("Message from " + num);
+        }
     }
 }
