18a90533568cc116a2b5900f3fddded67c00aa8b
Dashoshik <daryastarkova01@gmailcom>
2017-11-04 15:04:34 +0200

    OOP

---
+++ src/lesson14/OOPphrase.java
@@ -0,0 +1,15 @@
+package lesson14;
+
+/**
+ * Напишите метод, заменяющий в строке все вхождения «object oriented programming»(не учитываем регистр символов) на «OOP».
+ */
+public class OOPphrase {
+    public static void main(String[] args) {
+        String s = "Object Oriented proGRAMMing klgsng slggkm lsmlkdgm smgkmgklm lerokglm object oriented programming fdblmk dmgkgm";
+        System.out.println(changeTo());
+    }
+
+    static void changeTo(String s) {
+
+    }
+}
