33a518f598c602e6aaabe3c37f38e8268656d794
Dashoshik <daryastarkova01@gmailcom>
2017-11-05 13:51:23 +0200

    SumOfTwoNumbers

--- src/sumOfTwoNumbers/Main.java
+++ src/sumOfTwoNumbers/Main.java
@@ -5,14 +5,18 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("Введите два целых числа");
+        System.out.println("Введите два целых числа");
         Scanner scanner = new Scanner(System.in);
-        int a, b;
+        int a = 0, b = 0;
         if (scanner.hasNextInt()) {
             a = scanner.nextInt();
-            b = scanner.nextInt ();
-            System.out.println(a+b);
-        }else {
+        } else {
+            System.out.println("Вы ввели не целое число");
+        }
+        if (scanner.hasNextInt()) {
+            b = scanner.nextInt();
+            System.out.println(a + b);
+        } else {
             System.out.println("Вы ввели не целое число");
         }
     }
