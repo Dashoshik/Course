b972d9c28a8932da569574e99ce5cbc77130c868
Dashoshik <daryastarkova01@gmailcom>
2017-11-05 13:32:28 +0200

    Seasons отформатирован

--- src/lesson12/SeasonDemo.java
+++ src/lesson12/SeasonDemo.java
@@ -1,8 +1,5 @@
 package lesson12;
 
-/**
- *
- */
 public class SeasonDemo {
     public static void main(String[] args) {
         Seasons favourite = Seasons.SUMMER;
@@ -16,53 +13,29 @@ public class SeasonDemo {
     static public void definitionOfFavouriteSeason(Seasons favourite) {
         System.out.println("average temperature is a " + favourite.getTempAverage());
         switch (favourite) {
-
-            case WINTER: {
-
+            case WINTER:
                 System.out.println("I like WINTER!");
-
                 break;
-
-            }
-
-            case SPRING: {
-
+            case SPRING:
                 System.out.println("I like SPRING!");
-
                 break;
-
-            }
-
-            case SUMMER: {
-
+            case SUMMER:
                 System.out.println("I like SUMMER!");
-
                 break;
-
-            }
-            case AUTUMN: {
-
+            case AUTUMN:
                 System.out.println("I like AUTUMN!");
-
                 break;
-
-            }
         }
     }
 
     static public void cycleSeasons(Seasons[] allSeasons) {
         for (Seasons c : allSeasons) {
-
             System.out.println(c + " " + c.getTempAverage() + " " + c.getDescription());
-
-
         }
     }
 
     static public void mySeason() {
-
         Seasons vremyaGoda = Seasons.valueOf("SUMMER");
-
         System.out.println("Favourite season is a " + vremyaGoda);
     }
 }
