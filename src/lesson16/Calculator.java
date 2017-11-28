c757834b454892d5c5fb8402855776575196d29f
Dashoshik <daryastarkova01@gmailcom>
2017-11-28 21:58:20 +0200

    Calculator

--- src/lesson16/Calculator.java
+++ src/lesson16/Calculator.java
@@ -8,21 +8,21 @@ public class Calculator {
     }
 
     public static <T extends Number, V extends Number> double multiply(T t, V v) {
-        return (t.doubleValue() * v.doubleValue());//метод doubleValue() есть у всех классов оболочек типа Number
+        return t.doubleValue() * v.doubleValue();//метод doubleValue() есть у всех классов оболочек типа Number
     }
 
     public static <T extends Number, V extends Number> double divide(T t, V v) {
-        return (t.doubleValue() / v.doubleValue());//метод doubleValue() есть у всех классов оболочек типа Number
+        return t.doubleValue() / v.doubleValue();//метод doubleValue() есть у всех классов оболочек типа Number
     }
 
     public static <T extends Number, V extends Number> double subtraction(T t, V v) {
-        return (t.doubleValue() - v.doubleValue());//метод doubleValue() есть у всех классов оболочек типа Number
+        return t.doubleValue() - v.doubleValue();//метод doubleValue() есть у всех классов оболочек типа Number
     }
 
     public static void main(String[] args) {
         System.out.println(sum(10, 15.5));
         System.out.println(divide(2, 12.2));
         System.out.println(subtraction(5, 1));
-        System.out.println (multiply(2, 2.5));
+        System.out.println(multiply(2, 2.5));
     }
 }
